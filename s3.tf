92b786c407378eb14cb1c2d290c895e8a7174db9
Charles Painthamilchchelvan <34585788+charlesatm@users.noreply.github.com>
2023-10-25 07:10:27 +0000

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
 module "s3" {
   source                      = "./modules/s3"
-  tfstate_bucket_name         = "${var.project}-terraform-tfstates" #please change the bucket name in terraform.tf backend block as it does not support variables
-  lambda_s3_bucket_name       = "lambda-node-bucket"
-  codepipeline_bucket_name    = "${var.project}-codepipeline-buckets"
+  tfstate_bucket_name         = "${var.project}-tfstate-terraform" #please change the bucket name in terraform.tf backend block as it does not support variables
+  lambda_s3_bucket_name       = "nodejs-lambda-bucket"
+  codepipeline_bucket_name    = "${var.project}-awscodepipeline-bucket"
   lambda_s3_object_name       = "${var.func_name}.zip"
   lambda_source               = data.archive_file.zip_lambda.output_path
 }
